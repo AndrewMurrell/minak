a1a170d693c55141d2a1bf02c7892c587b766251
DavisLWebb <davislwebb@ymail.com>
2014-05-04 21:37:26 -0400

    did shit

--- src/us/minak/IMEView.java
+++ src/us/minak/IMEView.java
@@ -5,7 +5,30 @@ import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
 public class IMEView extends RelativeLayout{
+	private final Context mContext;
+	private OnCharacterEnteredListener mOnCharacterEnteredListener;
 	public IMEView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		mContext = context;
+	}
+
+	@Override
+	protected void onFinishInflate() {
+		DrawingSpaceView drawingSpaceView = (DrawingSpaceView) findViewById(R.id.drawing_space);
+		drawingSpaceView.setOnGestureRecognizedListener(new OnGestureRecognizedListener() {
+			@Override
+			public void gestureRecognized(String character) {
+				enterCharacter(character);
+			}
+
+		});
+	}
+
+	public void setOnCharacterEnteredListener(OnCharacterEnteredListener onCharacterEnteredListener) {
+		mOnCharacterEnteredListener = onCharacterEnteredListener;
+	}
+
+	private void enterCharacter(String character) {
+			mOnCharacterEnteredListener.characterEntered(character);
 	}
 }
